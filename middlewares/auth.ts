fc5b362a420ae546450e4b82548176477c936361
Jorge Matos <jisapjisap@gmail.com>
2026-07-15 20:44:31 +0200

    feat: implement JWT-based authentication middleware to protect routes

---
+++ middlewares/auth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express"
+import { IUser, User } from "../models/User.js"
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+  user?: IUser;
+}
+
+export const protect = async (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];                                  // Extraemos el token de los headers
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string }      // Verificamos el token y lo usamos como un id
+      const user = await User.findById(decoded.id).select('-password')                  // Buscamos el usuario y excluimos el password
+      if (!user) {
+        res.status(401).json({ message: "Not authorized, user not found" });
+        return;
+      }
+
+      req.user = user;
+      next();
+
+    } catch (error: any) {
+      console.log("Auth Middleware Error:", error)
+      res.status(401).json({ message: "Not Authorized, token failed." });
+      return;
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, No token" });
+    return;
+  }
+}
+
+
+
