18aa9adbe51d2179e825ae31f1a2b54097178f81
Jorge Matos <jisapjisap@gmail.com>
2026-07-16 17:08:51 +0200

    feat: add Restaurant Mongoose model and implement global error handling with auth routes

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import auhtRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -17,6 +18,17 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use("/api/auth", auhtRouter);
+
+// Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log("Unhandled Error: ", err);
+    res.status(500).json({
+        message: err.message || "Internal Server Error",
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
